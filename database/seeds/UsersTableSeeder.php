2c7bc63b24e674acc8d13649232c249a494e4ecb
AvazAlimov <avazalimov95@mail.ru>
2018-11-19 20:17:49 +0500

    Database Seeder Added: - UsersTableSeeder class

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("users")->insert([
+            "id" => 0,
+            "username" => "admin",
+            "password" => bcrypt('secret'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
